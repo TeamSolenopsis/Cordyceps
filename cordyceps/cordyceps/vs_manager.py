0c2a936f7c57d7b7aee2ad1d0af2982ef8759b1d
CasLeeflang <466933@student.fontys.nl>
2023-05-24 11:17:04 +0200

    CRD-26

--- cordyceps/cordyceps/vs_manager.py
+++ cordyceps/cordyceps/vs_manager.py
@@ -36,18 +36,14 @@ class Vs_manager(Node):
         self.RESOLUTION = 1000 # The amount of points in which the paths will be split.
         self.MAX_SPEED = 0.5 # Maximum allowed speed from a robot.(m/s)
 
-        
-        self.vs_origin_x = 400 / self.m2p  # pixels
-        self.vs_origin_y = 400 / self.m2p  # pixels
+        self.vs_origin_x = 0 / self.m2p  # pixels
+        self.vs_origin_y = 0 / self.m2p  # pixels
 
-        self.x = np.linspace(1.0, 5, self.RESOLUTION)
-        self.y_lins = np.linspace(1.0, 5, self.RESOLUTION)
-        self.y = (np.sin(self.y_lins) * 7)     
+        self.x, self.y = self.generate_vs_path(self.vs_origin_x, self.vs_origin_y)
 
         self.angle = 0.0  # rad
 
-        self.distance_to_vs_pixels = 50   # pixels
-        self.distance_to_vs = self.distance_to_vs_pixels / self.m2p   # meters
+        self.distance_to_vs = 1   # meters
 
         self.robot_paths = self.generate_paths(list(zip(self.x, self.y)))      
         self.plot_path(self.robot_paths, True)
@@ -81,10 +77,10 @@ class Vs_manager(Node):
             )  
 
             # Robot coordinates.
-            bot_0_xy = np.array([self.distance_to_vs, -self.distance_to_vs, 1])
-            bot_1_xy = np.array([self.distance_to_vs, self.distance_to_vs, 1])
-            bot_2_xy = np.array([-self.distance_to_vs, self.distance_to_vs, 1])
-            bot_3_xy = np.array([-self.distance_to_vs, -self.distance_to_vs, 1])
+            bot_0_xy = np.array([self.distance_to_vs, 0, 1])
+            bot_1_xy = np.array([0, self.distance_to_vs, 1]) 
+            bot_2_xy = np.array([-self.distance_to_vs, 0, 1])
+            bot_3_xy = np.array([0, -self.distance_to_vs, 1])
 
             # Calculation for pose of every robot in the VS.
             trans_0 = tf_matrix.dot(bot_0_xy)
@@ -120,14 +116,28 @@ class Vs_manager(Node):
 
         return path        
 
+    def generate_vs_path(self, vs_origin_x, vs_origin_y) -> tuple():
+        x = [vs_origin_x]
+        y = [vs_origin_y]
 
+        # right
+        x = np.append(x, np.linspace(x[-1] - 1, x[-1], self.RESOLUTION))
+        y = np.append(y, np.linspace(y[-1],y[-1], self.RESOLUTION))
 
-    def plot_path(self, path, show:bool):
+        # turn right
+        r = 4
+        i = np.linspace(0*np.pi, 0.5*np.pi, self.RESOLUTION)
+        x = np.append(x, (x[-1]) + np.flip(np.cos(i)*r))
+        y = np.append(y, (y[-1]- r) + np.flip(np.sin(i)*r))
+
+        return x, y
+
+    def plot_path(self, path, show:bool) -> None:
         li_r1 = []
         li_r2 = []
         li_r3 = []
         li_r4 = []
-        for i, pose in enumerate(path):
+        for pose in path:
             li_r1.append([pose[0][0], pose[0][1]])
             li_r2.append([pose[1][0], pose[1][1]])
             li_r3.append([pose[2][0], pose[2][1]])
@@ -138,8 +148,11 @@ class Vs_manager(Node):
         plt.scatter(*zip(*li_r3), s=3)
         plt.scatter(*zip(*li_r4), s=3)
 
+        plt.legend(['R1', 'R2', 'R3', 'R4'])
+
         if show:       
             plt.show()
+
 
 
 def main(args=None):
