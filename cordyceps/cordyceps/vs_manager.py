b2c9c2d13370b913a69edcdeb80c1b1afa19c305
CasLeeflang <466933@student.fontys.nl>
2023-05-24 14:54:36 +0200

    CRD-43

--- cordyceps/cordyceps/vs_manager.py
+++ cordyceps/cordyceps/vs_manager.py
@@ -1,5 +1,9 @@
 import rclpy
 from rclpy.node import Node
+import numpy as np
+import matplotlib.pyplot as plt
+from geometry_msgs.msg import Pose
+from cordyceps_interfaces.srv import CustomPathPlanner
 
 # from cordyceps_interfaces.msg import arrived
 # from cordyceps_interfaces.msg import pose_shape
@@ -8,7 +12,38 @@ class Vs_manager(Node):
 
     def __init__(self):
         super().__init__('vs_manager')
-       
+        self.robot_path_client = self.create_client(CustomPathPlanner, 'get_robot_paths')
+        self.req = CustomPathPlanner.Request()
+        print(self.send_request(0.5))
+
+    def send_request(self, p:float):
+        self.req.p = p
+        self.future = self.robot_path_client.call_async(self.req)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+
+    def plot_path(self, path, show:bool) -> None:
+        li_r1 = []
+        li_r2 = []
+        li_r3 = []
+        li_r4 = []
+        for poses in path:
+            li_r1.append([poses[0][0], poses[0][1]])
+            li_r2.append([poses[1][0], poses[1][1]])
+            li_r3.append([poses[2][0], poses[2][1]])
+            li_r4.append([poses[3][0], poses[3][1]])
+            
+        plt.scatter(*zip(*li_r1), s=3)
+        plt.scatter(*zip(*li_r2), s=3)
+        plt.scatter(*zip(*li_r3), s=3)
+        plt.scatter(*zip(*li_r4), s=3)
+
+        plt.legend(['R1', 'R2', 'R3', 'R4'])
+
+        if show:       
+            plt.show()
+
+
 
 def main(args=None):
     rclpy.init(args=args)
