b4569160c7da49f6428016b1a47dfc5d0a68cd89
Mart Coppelmans <45392496+CrazyPandabier@users.noreply.github.com>
2023-05-26 09:36:20 +0200

    Update cordyceps/cordyceps/vs_manager.py
    Co-authored-by: SaraEersel <71814295+SaraEersel@users.noreply.github.com>

--- cordyceps/cordyceps/vs_manager.py
+++ cordyceps/cordyceps/vs_manager.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 
 from rclpy.action import ActionClient
 
-# from cordyceps_interfaces.msg import Arrived
 # from cordyceps_interfaces.msg import PoseShape
 # from cordyceps_interfaces.msg import Assembled  
 
