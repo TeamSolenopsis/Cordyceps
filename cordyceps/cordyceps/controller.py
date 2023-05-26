8141c1528c1d4ee5900cd5523c2d45a24c2269b3
Mart Coppelmans <mart.coppelmans@gmail.com>
2023-05-26 15:18:14 +0200

    fixed threads running in vs manger moved plot show to controller

--- cordyceps/cordyceps/controller.py
+++ cordyceps/cordyceps/controller.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionServer
+import numpy as np
+import matplotlib.pyplot as plt
 
 from cordyceps_interfaces.action import Controller
 
@@ -11,10 +13,40 @@ class ControllerActionServer(Node):
         
     def execute_callback(self, goal_handle):
         self.get_logger().info('Executing goal...')
+        self.plot_path(goal_handle.request.robot_paths, False)
         goal_handle.succeed()
         result = Controller.Result()
         result.done = True
         return result
+    
+    def plot_path(self, robot_paths, show:bool) -> None:
+
+        path_r1 = []
+        path_r2 = []
+        path_r3 = []
+        path_r4 = []
+
+        for poses in robot_paths.paths[0].robot_poses[:]:
+            path_r1.append([poses.x, poses.y])
+
+        for poses in robot_paths.paths[1].robot_poses[:]:
+            path_r2.append([poses.x, poses.y])
+        
+        for poses in robot_paths.paths[2].robot_poses[:]:
+            path_r3.append([poses.x, poses.y])
+
+        for poses in robot_paths.paths[3].robot_poses[:]:
+            path_r4.append([poses.x, poses.y])
+            
+        plt.scatter(*zip(*path_r1), s=3)
+        plt.scatter(*zip(*path_r2), s=3)
+        plt.scatter(*zip(*path_r3), s=3)
+        plt.scatter(*zip(*path_r4), s=3)
+
+        plt.legend(['R1', 'R2', 'R3', 'R4'])
+
+        if show:       
+            plt.show()
 
 def main(args=None):
     rclpy.init(args=args)
--- cordyceps/cordyceps/vs_manager.py
+++ cordyceps/cordyceps/vs_manager.py
@@ -1,7 +1,5 @@
 import rclpy
 from rclpy.node import Node
-import numpy as np
-import matplotlib.pyplot as plt
 import threading
 from queue import Queue
 from geometry_msgs.msg import Pose
@@ -10,7 +8,7 @@ from rclpy.action import ActionClient
 
 from cordyceps_interfaces.action import Controller
 from cordyceps_interfaces.srv import CustomPathPlanner, CustomRobotAssembler
-from cordyceps_interfaces.msg import RobotPaths, Task
+from cordyceps_interfaces.msg import RobotPaths, Task, RobotPose
 
 class Vs_manager(Node):
 
@@ -25,74 +23,44 @@ class Vs_manager(Node):
         self.controller_action_client = ActionClient(self, Controller, 'controller')
         self.task_subscriber = self.create_subscription(Task, 'vs_manager/task', self.task_callback, 10)
 
-        self.assembler_response = self.send_assembler_request(self.construct_mock_task())
-        self.path_planner_response = self.send_path_planner_request(self.construct_mock_task(), self.assembler_response.vs_ref_pose)
-        self.assembler_response = self.send_assembler_request(self.construct_mock_task())
-        self.plot_path(self.path_planner_response, True)
+        #temp
+        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.pub = self.create_publisher(Task, 'vs_manager/task', 10)
 
 
+    def timer_callback(self):
+        task = self.construct_mock_task()
+        self.pub.publish(task)
 
     def task_callback(self, msg:Task):
         self.task_queue.put(msg)
 
-    def send_path_planner_request(self, task, vs_ref_pose):
-        planner_request = CustomPathPlanner.Request()
-
     def task_executor(self):
         while True:
             task = self.task_queue.get(block=True)
-            print("Task received")
-            paths = self.request_paths(task)
+            vs_ref_pose = self.request_vs_ref_pose(task)
+            paths = self.request_paths(task, vs_ref_pose)
             self.controll_vs(paths)
 
-
-    def request_paths(self, task:Task):
+    def request_paths(self, task:Task, vs_ref_pose:list):
         req  = CustomPathPlanner.Request()
         req.task = task
         req.vs_ref_pose = vs_ref_pose
 
-        future = self.robot_path_client.call_async(self.req)
-        rclpy.spin_until_future_complete(self, future)
-        return future.result()
+        while True:
+            if self.robot_path_client.wait_for_service():
+                break
+        respone = self.robot_path_client.call(req)
+        return respone.robot_paths
     
-    def send_assembler_request(self, task):
+    def request_vs_ref_pose(self, task):
         assembler_request = CustomRobotAssembler.Request()
-
         assembler_request.task = task
-
-        response = self.assembler_client.call_async(assembler_request)
-
-        rclpy.spin_until_future_complete(self, response)
-        return response.result()
-
-    def plot_path(self, path, show:bool) -> None:
-
-        path_r1 = []
-        path_r2 = []
-        path_r3 = []
-        path_r4 = []
-
-        for poses in self.path_planner_response.robot_paths.paths[0].robot_poses[:]:
-            path_r1.append([poses.x, poses.y])
-
-        for poses in self.path_planner_response.robot_paths.paths[1].robot_poses[:]:
-            path_r2.append([poses.x, poses.y])
-        
-        for poses in self.path_planner_response.robot_paths.paths[2].robot_poses[:]:
-            path_r3.append([poses.x, poses.y])
-
-        for poses in self.path_planner_response.robot_paths.paths[3].robot_poses[:]:
-            path_r4.append([poses.x, poses.y])
-            
-        plt.scatter(*zip(*path_r1), s=3)
-        plt.scatter(*zip(*path_r2), s=3)
-        plt.scatter(*zip(*path_r3), s=3)
-        plt.scatter(*zip(*path_r4), s=3)
-
-        plt.legend(['R1', 'R2', 'R3', 'R4'])
-
-        if show:       
-            plt.show()
+        while True:
+            if self.assembler_client.wait_for_service():
+                break
+        response = self.assembler_client.call(assembler_request)
+        return response.vs_ref_pose
 
     def construct_mock_task(self) -> Task:
         task = Task()
@@ -109,8 +77,9 @@ class Vs_manager(Node):
         goal_msg.robot_paths = paths
 
         self.controller_action_client.wait_for_server()
-        future = self.controller_action_client.send_goal_async(goal_msg)
-        return future.result()
+        response = self.controller_action_client.send_goal(goal_msg)
+        print(response)
+        return response
 
 
 def main(args=None):
