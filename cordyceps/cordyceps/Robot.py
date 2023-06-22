864f9754d6755f003507886f1e1546b7712901e4
TangoUniform <460642@student.fontys.nl>
2023-06-22 15:45:53 +0200

    Controller v2 added

--- cordyceps/cordyceps/Robot.py
+++ cordyceps/cordyceps/Robot.py
@@ -6,54 +6,108 @@ import math
 import threading
 import time
 
+
 class Robot:
-    def __init__(self, x, y, theta, name, node:Node) -> None:
+    def __init__(self, x, y, theta, name, node: Node) -> None:
         self.node = node
         self.name = name
         self.lock = threading.Lock()
 
-        self.pose_sub = self.node.create_subscription(Odometry, f'/{self.name}/odom', self.odom_callback, 10)
-        self.cmd_vel_pub = self.node.create_publisher(Twist, f'/{self.name}/cmd_vel', 10)
+        self.pose_sub = self.node.create_subscription(
+            Odometry, f"/{self.name}/odom", self.odom_callback, 10
+        )
+        self.cmd_vel_pub = self.node.create_publisher(
+            Twist, f"/{self.name}/cmd_vel", 10
+        )
 
-        self.pose = np.array([[float(x),float(y),float(theta)]]).T
+        self.pose = np.array([[float(x), float(y), float(theta)]]).T
+        self.LOOKAHEAD = 8  # number of points pure pursuit looks ahead
 
-    def odom_callback(self, msg:Odometry):
+        self._prev_point_index = 0
+
+    def set_prev_point_index(self, prev_point_index: int):
+        self._prev_point_index = prev_point_index
+
+    def get_prev_point_index(self):
+        return self._prev_point_index
+
+    def odom_callback(self, msg: Odometry):
         with self.lock:
-            self.pose[0][0] = round(msg.pose.pose.position.x,2)
-            self.pose[1][0] = round(msg.pose.pose.position.y,2)
+            self.pose[0][0] = round(msg.pose.pose.position.x, 2)
+            self.pose[1][0] = round(msg.pose.pose.position.y, 2)
 
-            w, x, y, z = msg.pose.pose.orientation.w, msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z
+            w, x, y, z = (
+                msg.pose.pose.orientation.w,
+                msg.pose.pose.orientation.x,
+                msg.pose.pose.orientation.y,
+                msg.pose.pose.orientation.z,
+            )
             siny_cosp = 2 * (w * z + x * y)
             cosy_cosp = 1 - 2 * (y * y + z * z)
-            yaw = round(math.atan2(siny_cosp, cosy_cosp),2)
-
+            yaw = round(math.atan2(siny_cosp, cosy_cosp), 2)
             self.pose[2][0] = yaw
 
-    def get_point_ref_to_robot_frame(self, point:np.array([[float, float, float]]).T):
+    def project_pose(self, prev_point_index, route: list):
+        """returns the index of the closest point in the route to the coordinates"""
+
+        # calculate displacements between the prev_point and the nearby next
+        displacement = []
+        for point in route[prev_point_index : (prev_point_index + self.LOOKAHEAD + 5)]:
+            coordinates = np.array((self.pose[0][0], self.pose[1][0]))
+            displacement.append(np.linalg.norm(coordinates - point))
+            # print(f"from {tt} to {point} there's {displacement[-1]} meters")
+
+        # calculate the index of the closest
+        min_displacement_index = prev_point_index + displacement.index(
+            min(displacement)
+        )
+
+        print(
+            f"{self.name}, min_displacement: {min_displacement_index}, {prev_point_index}"
+        )
+
+        return min_displacement_index
+
+    def calculate_carrot(self, projected_point_index: int, route: list):
+        """given the projected position of a bot in its route, returns the goal (aka lookahead point)"""
+
+        carrot = route[projected_point_index + self.LOOKAHEAD]
+        print(f"lookahead_point: {projected_point_index + self.LOOKAHEAD}")
+        return carrot
+
+    def get_point_ref_to_robot_frame(self, point: np.array([[float, float, float]]).T):
         with self.lock:
             rev_point = -self.pose
 
-            trans_matrix = np.array([[1, 0, rev_point[0][0]], [0, 1, rev_point[1][0]], [0, 0, 1]])
+            trans_matrix = np.array(
+                [[1, 0, rev_point[0][0]], [0, 1, rev_point[1][0]], [0, 0, 1]]
+            )
             new_point = np.matmul(trans_matrix, point)
-            rot_matrix = np.array([[np.cos(rev_point[2][0]), -np.sin(rev_point[2][0]), 0], [np.sin(rev_point[2][0]), np.cos(rev_point[2][0]), 0], [0, 0, 1]])
+            rot_matrix = np.array(
+                [
+                    [np.cos(rev_point[2][0]), -np.sin(rev_point[2][0]), 0],
+                    [np.sin(rev_point[2][0]), np.cos(rev_point[2][0]), 0],
+                    [0, 0, 1],
+                ]
+            )
             new_point = np.matmul(rot_matrix, new_point)
             return new_point
 
-    def get_deltas(self, goal:np.array([[float,float,float]]).T) -> float:
+    def get_deltas(self, goal: np.array([[float, float, float]]).T) -> float:
         goal = self.get_point_ref_to_robot_frame(goal)
-        displacement = float(np.sqrt(goal[0][0]**2 + goal[1][0]**2))
+        displacement = np.hypot(goal[0], goal[1], dtype=float)
 
         if displacement == 0.0:
             return 0.0, 0.0, 0.0
-        if goal[1][0] == 0:
-            return displacement,0.0, displacement
+        if goal[1] == 0:
+            return displacement, 0.0, displacement
 
-        radius = displacement **2 / (2 * goal[1][0])
-        delta_theta = 2 * np.arcsin(displacement/ (2 * radius))
+        radius = displacement**2 / (2 * goal[1])
+        delta_theta = 2 * np.arcsin(displacement / (2 * radius))
         delta_s = delta_theta * radius
         return delta_s, delta_theta, displacement
-    
-    def publish_velocity(self, lin_vel:float, ang_vel:float):
+
+    def publish_velocity(self, lin_vel: float, ang_vel: float):
         msg = Twist()
         msg.linear.x = lin_vel
         msg.angular.z = ang_vel
