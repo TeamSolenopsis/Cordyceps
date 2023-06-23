e9b64d89439aed4f2f531ac527e31b6005850589
SaraEersel <71814295+SaraEersel@users.noreply.github.com>
2023-06-23 11:44:36 +0200

    Update cordyceps/cordyceps/path_planner.py

--- cordyceps/cordyceps/path_planner.py
+++ cordyceps/cordyceps/path_planner.py
@@ -21,7 +21,7 @@ class PathPlanner(Node):
 
     def generate_vs_path_mock(self, start_pose:Pose) -> np.array:
 
-        file = open('/home/tangouniform/Documents/cordycepsws/Turtlebot3_Simulation_WorkSpace/turtle_ws/src/Cordyceps/cordyceps/resource/Path1.csv','r')
+        file = open('cordyceps/resource/Path1.csv','r')
         data = list(csv.reader(file, delimiter=','))
         file.close()
         
